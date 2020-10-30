f218becf2a3b5a88c321d9a828dd1dcd825fcfd3
Bev Cardinale <mc6672@cs.ship.edu>
2020-10-29 23:15:51 -0400

    main method outlined

--- main.c
+++ main.c
@@ -1,8 +1,53 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 int main() {
-    //TODO
 
+    // read in a line of stuff -> input
+
+    char *instruction = normalize(input);
+
+    char *mnemonic = parse_string(instruction); // TODO - Yong returns the mnemonic (up til first comma)
+
+    instr_t instr = validate_instruction(mnemonic);    // TODO - Nicholas returns the instruction as an instr_t struct
+
+    if (instr == null) {
+        printf("ERROR - INVALID INSTRUCTION\n");
+        goto done;
+    }
+
+    typedef enum {
+        PARSE_RTYPE, PARSE_ITYPE, PARSE_JTYPE, PARSE_JRTYPE, MEMTYPE, SHIFTY
+    } instr_type_t;
+
+    uint32_t formatted_instr;
+    
+    switch (instr.type) {
+        case PARSE_RTYPE:
+            formatted_instr = format_rtype();
+            break;
+        case PARSE_ITYPE:
+            formatted_instr = format_itype();
+            break;
+        case PARSE_JTYPE:
+            formatted_instr = format_jtype();
+            break;
+        case PARSE_JRTYPE:
+            formatted_instr = format_jrtype();
+            break;
+        case MEMTYPE:
+            formatted_instr = format_memtype();
+            break;
+        case SHIFTY:
+            formatted_instr = format_shifty();
+    }
+
+    if (formatted_instr == null) {
+        printf("ERROR - INVALID REGISTER\n");
+        goto done;
+    }
+
+    printf("%d", converttohex(formatted_instr));
+
+done:
     return 0;
 }
--- main.h
+++ main.h
@@ -1,3 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUM_INSTR ( sizeof(instrs) / sizeof(instr_t))
+
+typedef enum {
+    FMT_RTYPE, FMT_ITYPE, FMT_JTYPE
+} encode_type_t;
+
+typedef struct {
+    char *mnemonic;
+    instr_type_t type;
+    encode_type_t etype;
+    int opcode;
+    int function;
+} instr_t;
+
+typedef struct {
+    int last_state;
+    instr_t instr;
+    char *opcode;
+    char *regs[3];
+    char *immediate;
+    char *shamt;
+} parsed_t;
+
+uint32_t format_rtype(parsed_t *parsed)
+{
+    int code = lookup_opcode(parsed->opcode);
+    int rd = decode_register(parsed->regs[0]);
+    int rs = decode_register(parsed->regs[1]);
+    int rt = decode_register(parsed->regs[2]);
+    int shamt = strtol(parsed->shamt, NULL, 0);
+    int func = lookup_func(parsed->opcode);
+
+    return code << 26 | rs << 21 | rt << 16 | rd << 11 | shamt << 5 | func;
+}
+
+int find_opcode(const char *str, instr_t *instr)
+{
+    int i;
+    for (i = 0; i < NUM_INSTR; i++) {
+        if (strcmp(instrs[i].mnemonic, str) == 0) {
+            *instr = instrs[i];
+            return 0;
+        }
+    }
+}
+
+
+
+
+
+
+
 typedef struct r_type {
     unsigned int opcode = 0b000000;
     unsigned int rs:5;
